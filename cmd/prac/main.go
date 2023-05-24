3c103e0d18b48ed15e3d08bd25e2d03f489251c0
snowmerak <snowmerak@outlook.com>
2023-05-25 01:07:17 +0900

    update example

--- cmd/prac/main.go
+++ cmd/prac/main.go
@@ -3,42 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/snowmerak/errors/lib/bufferedlist"
+	"github.com/snowmerak/errors/lib/formatter"
 	"io"
 )
 
 func main() {
+	test()
+}
+
+func test() {
 	bl := bufferedlist.New()
-	for i := 0; i < 8000; i++ {
-		bl.WriteByte(byte(i % 256))
+
+	jf, err := formatter.NewJsonFormatter(bl)
+	if err != nil {
+		panic(err)
 	}
 
-	buf := make([]byte, 3000)
+	jf.Byte("byte", 'a')
+	jf.Int64("int64", 64)
+	jf.Uint64("uint64", 64)
+	jf.Float64("float64", 64.0)
+	jf.String("string", "string")
+	jf.Bool("bool", true)
+	jf.Err("err", nil)
+	jf.Caller()
+	jf.Msg("msg")
 
 	reader := bl.Reader()
-	n, err := reader.Read(buf)
-	if err != nil && err != io.EOF {
-		panic(err)
+	buf := [1024]byte{}
+	for {
+		n, err := reader.Read(buf[:])
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+		fmt.Println(string(buf[:n]))
 	}
-
-	fmt.Println(n)
-	fmt.Println(buf[:n])
-	fmt.Println("----------")
-
-	n, err = reader.Read(buf)
-	if err != nil && err != io.EOF {
-		panic(err)
-	}
-
-	fmt.Println(n)
-	fmt.Println(buf[:n])
-	fmt.Println("----------")
-
-	n, err = reader.Read(buf)
-	if err != nil && err != io.EOF {
-		panic(err)
-	}
-
-	fmt.Println(n)
-	fmt.Println(buf[:n])
-	fmt.Println("----------")
 }

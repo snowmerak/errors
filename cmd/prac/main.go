af58fec46e73fe92f844c820d43c68cc575f2193
snowmerak <snowmerak@outlook.com>
2023-05-25 01:36:25 +0900

    update example

--- cmd/prac/main.go
+++ cmd/prac/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/snowmerak/errors/lib/bufferedlist"
+	"github.com/snowmerak/errors/lib/errors"
 	"github.com/snowmerak/errors/lib/formatter"
-	"io"
 )
 
 func main() {
@@ -12,33 +11,8 @@ func main() {
 }
 
 func test() {
-	bl := bufferedlist.New()
-
-	jf, err := formatter.NewJsonFormatter(bl)
-	if err != nil {
-		panic(err)
-	}
-
-	jf.Byte("byte", 'a')
-	jf.Int64("int64", 64)
-	jf.Uint64("uint64", 64)
-	jf.Float64("float64", 64.0)
-	jf.String("string", "string")
-	jf.Bool("bool", true)
-	jf.Err("err", nil)
-	jf.Caller()
-	jf.Msg("msg")
-
-	reader := bl.Reader()
-	buf := [1024]byte{}
-	for {
-		n, err := reader.Read(buf[:])
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		if n == 0 {
-			break
-		}
-		fmt.Println(string(buf[:n]))
-	}
+	err := errors.New(formatter.NewJsonFormatter)
+	err.Caller()
+	err.Msg("hello")
+	fmt.Println(err.Error())
 }

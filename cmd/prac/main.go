7f729fb22310d28068eb61fa574c8032f8cc6997
snowmerak <snowmerak@outlook.com>
2023-05-25 18:35:07 +0900

    Update main.go

--- cmd/prac/main.go
+++ cmd/prac/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/snowmerak/errors/lib/errors"
 	"github.com/snowmerak/errors/lib/formatter"
+	"os"
 )
 
 func main() {
@@ -11,8 +12,8 @@ func main() {
 }
 
 func test() {
-	err := errors.New(formatter.NewJsonFormatter)
-	err.Caller()
-	err.Msg("hello")
-	fmt.Println(err.Error())
+	pErr := errors.New(formatter.NewJsonFormatter).Int64("hello", 123).Msg("hello")
+
+	errors.New(formatter.NewJsonFormatter).Caller().Err("hello", pErr).Msg("hello").MoveTo(os.Stdout)
+	fmt.Println()
 }
